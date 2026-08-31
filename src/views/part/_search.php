94961e2b29e641392dd8981399a5b7ca0848fe6e
merovingian <vadimgrechuha@gmail.com>
2026-08-31 19:04:23 +0300

    HQD-388: Remove device_location field from Advanced Search
    The field's purpose was unclear and it duplicated other location-related
    filters (rack, Location tree). Only the owner-staff-only Advanced Search
    field is removed; the DC location grid column and its inline filter,
    which are used by all users, are left as is.

--- src/views/part/_search.php
+++ src/views/part/_search.php
@@ -108,18 +108,6 @@ JS
     </div>
 <?php endif ?>
 
-<?php if (Yii::$app->user->can('owner-staff')) : ?>
-    <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('device_location')->widget(
-            SearchManagedField::class,
-            [
-                'searchBy' => [SearchBy::LIKEI, SearchBy::LEFT_LIKEI],
-                'default' => SearchBy::LIKEI,
-            ]
-        ) ?>
-    </div>
-<?php endif ?>
-
 <div class="col-md-4 col-sm-6 col-xs-12">
     <?= $search->field('place_in')->widget(LocationsCombo::class, ['multiple' => true]) ?>
 </div>
